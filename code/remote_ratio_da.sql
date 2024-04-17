a3129dcdd2a06f3e444c59f7cb92592972a2f7b3
Nirawit Pongpisut <144346077+iintime@users.noreply.github.com>
2024-04-17 16:20:36 +0700

    Create remote_ratio_da.sql
    Remote Ratio of Data Analyst.

---
+++ code/remote_ratio_da.sql
@@ -0,0 +1,6 @@
+SELECT job_title,
+	   remote_ratio,
+	   count(remote_ratio) as number_of_employee
+FROM ds_salaries
+WHERE job_title = "Data Analyst" and company_location = "US" 
+GROUP BY remote_ratio
