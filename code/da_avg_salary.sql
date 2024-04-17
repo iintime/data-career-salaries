5a2f3c189e383b645fdc969773217cfccc7fd8fa
Nirawit Pongpisut <144346077+iintime@users.noreply.github.com>
2024-04-17 15:45:47 +0700

    Update da_avg_salary.sql

--- code/da_avg_salary.sql
+++ code/da_avg_salary.sql
@@ -1,7 +1,7 @@
 SELECT job_title,
-	   experience_level, 
-	   company_location, 
-	   round(avg(salary_in_usd)) as avg_salary_in_usd
+       experience_level, 
+       company_location, 
+       round(avg(salary_in_usd)) as avg_salary_in_usd
 FROM ds_salaries
 WHERE job_title = "Data Analyst" and company_location = "US"
 GROUP by job_title, experience_level;
