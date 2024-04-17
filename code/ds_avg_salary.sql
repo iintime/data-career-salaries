73149007615ff5c4ef2af474cfaa332a3aff78a5
Nirawit Pongpisut <144346077+iintime@users.noreply.github.com>
2024-04-17 15:49:59 +0700

    Create ds_avg_salary.sql
    Average Salary of Data Scientist in the US

---
+++ code/ds_avg_salary.sql
@@ -0,0 +1,7 @@
+SELECT job_title,
+	   experience_level, 
+	   company_location,
+	   round(avg(salary_in_usd)) as avg_salary_in_usd
+FROM ds_salaries
+WHERE job_title = "Data Scientist" and company_location = "US"
+GROUP by job_title, experience_level;
